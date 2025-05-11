7543e82b50b6ab54e195f542efdbd1adacc32e27
Luka123-dot <l_ka3@Icloud.com>
2025-05-11 13:10:11 -0400

    a

--- day28/homework/index.py
+++ day28/homework/index.py
@@ -1,8 +1,6 @@
 #Write a while loop that stops running if the number becomes 3:
-num = int(input('enter number here:'))
 
-while num == 3:
-    if num == 3:
-        break
-    else:
-        print('wrong')
+num = 0
+while num < 3:
+    print(num)
+    num += 1 
--- day28/homework/index2.py
+++ day28/homework/index2.py
@@ -4,8 +4,4 @@ def true_if_10():
     num = int(input('enter number here:'))
     if num > 10:
         print(True)
-    elif num < 10:
-        print(False)
-    elif num == 10:
-        print(False)
 true_if_10()
